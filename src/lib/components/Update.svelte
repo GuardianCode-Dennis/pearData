1aefb8687952d820c1423cb7d50795ec0e22bc3a
Dennis Betman <dennisbetman@Denniss-MacBook-Air.local>
2025-05-29 23:43:15 +0200

    Fix

--- src/lib/components/Update.svelte
+++ src/lib/components/Update.svelte
@@ -1,26 +1,33 @@
 <script>
-    let timeLeft = 300;
-    let formattedTime = formatTime(timeLeft);
+	import { onMount } from 'svelte';
 
-    function formatTime(seconds) {
-        const minutes = Math.floor(seconds / 60);
-        const remainingSeconds = seconds % 60;
-        const formattedMinutes = String(minutes).padStart(2, '0');
-        const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-        return `${formattedMinutes}:${formattedSeconds}`;
-    }
+	let timeLeft = 300;
+	let formattedTime = formatTime(timeLeft);
+	let intervalId;
 
-    function updateTime() {
-        setInterval(() => {
-            timeLeft--;
-            if (timeLeft < 1) {
-                timeLeft = 300;
-            }
-            formattedTime = formatTime(timeLeft);
-        }, 1000);
-    }
+	function formatTime(seconds) {
+		const minutes = Math.floor(seconds / 60);
+		const remainingSeconds = seconds % 60;
+		const formattedMinutes = String(minutes).padStart(2, '0');
+		const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+		return `${formattedMinutes}:${formattedSeconds}`;
+	}
 
-    updateTime();
+	onMount(() => {
+		intervalId = setInterval(() => {
+			timeLeft--;
+			if (timeLeft < 1) {
+				timeLeft = 300;
+			}
+			formattedTime = formatTime(timeLeft);
+		}, 1000);
+
+		// Zorg voor cleanup als component wordt verwijderd
+		return () => clearInterval(intervalId);
+	});
 </script>
 
-<span class="text-sm"><span class="hidden md:inline">Next update in:</span> <strong class="w-[40px] inline-block text-left justify-center relative top-[0.5px]">{formattedTime}</strong></span>
+<span class="text-sm">
+	<span class="hidden md:inline">Next update in:</span>
+	<strong class="w-[40px] inline-block text-left justify-center relative top-[0.5px]">{formattedTime}</strong>
+</span>
